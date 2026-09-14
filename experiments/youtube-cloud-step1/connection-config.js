fc74b5bfe8420907d6c54b754fe7982c99056176
20251014peru-gif <20251014peru@gmail.com>
2026-09-14 12:44:22 +0900

    Add isolated YouTube v38 copy with configurable service connections

---
+++ experiments/youtube-cloud-step1/connection-config.js
@@ -0,0 +1,8 @@
+/* 배포 시에만 편집하는 연결 설정. 비밀키를 이 파일에 넣지 마세요.
+ * 빈 apiBase는 현재 웹 서버를 사용합니다(file://에서는 기존 localhost:5055).
+ * 아래 주소를 설정해도 서버·인증·작업 큐가 자동으로 만들어지지는 않습니다.
+ */
+window.YT_CONNECTION_CONFIG = Object.freeze({
+  apiBase: '',
+  captureBase: ''
+});
